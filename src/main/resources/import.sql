4c2987fe2d0be9f80392c68984d486441d721d80
Kai Herwijn <kai.herwijn@additude.eu>
2020-06-11 22:27:31 +0200

    Extended the import script with some extra stuff and members.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,10 +2,22 @@ insert into stuff(name) values('Trein');
 insert into stuff(name) values('Sjoelbak');
 insert into stuff(name) values('Loopauto');
 insert into stuff(name) values('Dierentuin');
+insert into stuff(name) values('Weefgetouw');
+insert into stuff(name) values('Dambord');
+insert into stuff(name) values('Speelhuisje');
+insert into stuff(name) values('Skelter');
+insert into stuff(name) values('Voetbaldoeltjes');
+insert into stuff(name) values('Springtouw');
 
 insert into member(name) values('Laurus');
 insert into member(name) values('Enola');
 insert into member(name) values('Stefan');
 insert into member(name) values('Franka');
+insert into member(name) values('Henk');
+insert into member(name) values('Sven');
+insert into member(name) values('Gertjan');
+insert into member(name) values('Sandra');
+insert into member(name) values('Kim');
+insert into member(name) values('Ingrid');
 
 insert into loaning(take, bring, stuff_id, member_id) values('2020-05-12', '2020-05-20', 1, 1);
